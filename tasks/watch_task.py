2ecc54ff4b13aa9773fdc3cf97cee6ec94d33ad8
EX-MG-TX <80203485+EX-MG-TX@users.noreply.github.com>
2021-03-10 18:17:37 +0800

    Update watch_task.py

--- tasks/watch_task.py
+++ tasks/watch_task.py
@@ -18,7 +18,7 @@ async def watch_task(biliapi: asyncbili) -> None:
         return
     ids = [{"aid":"204516234","cid":"307178951"},{"aid":"756904275","cid":"304166415"},{"aid":"204502833","cid":"307178835"},{"aid":"459368214","cid":"299736533"}]
     try:
-	nub = random.randint(0,3);
+	nub = random.randint(0,3)
 	ret = await biliapi.report(ids[nub]["aid"], ids[nub]["cid"], 300)
         if ret["code"] == 0:
             logging.info(f'{biliapi.name}: 成功模拟观看av号为{ids[nub]["aid"]}的视频')
