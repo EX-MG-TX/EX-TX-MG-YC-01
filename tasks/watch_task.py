5be986467f29abb770031b9f11b670ee13cf25fe
EX-MG-TX <80203485+EX-MG-TX@users.noreply.github.com>
2021-03-10 18:07:20 +0800

    Update watch_task.py

--- tasks/watch_task.py
+++ tasks/watch_task.py
@@ -2,6 +2,7 @@ from BiliClient import asyncbili
 from .push_message_task import webhook
 from .import_once import get_ids
 import logging
+import random
 
 async def watch_task(biliapi: asyncbili) -> None:
     try:
@@ -15,13 +16,14 @@ async def watch_task(biliapi: asyncbili) -> None:
         logging.warning(f'{biliapi.name}: 获取B站分区视频信息异常，原因为{ids["message"]}，跳过视频观看')
         webhook.addMsg('msg_simple', f'{biliapi.name}:模拟视频观看失败\n')
         return
-    ids = ret["data"]["archives"]
+    ids = [{"aid":"204516234","cid":"307178951"},{"aid":"756904275","cid":"304166415"},{"aid":"204502833","cid":"307178835"},{"aid":"459368214","cid":"299736533"}]
     try:
-        ret = await biliapi.report(756904275, 304166415, 300)
+		nub = random.randint(0,3);
+		ret = await biliapi.report(ids[nub]["aid"], ids[nub]["cid"], 300)
         if ret["code"] == 0:
-            logging.info(f'{biliapi.name}: 成功模拟观看av号为{ids[5]["aid"]}的视频')
+            logging.info(f'{biliapi.name}: 成功模拟观看av号为{ids[nub]["aid"]}的视频')
         else:
-            logging.warning(f'{biliapi.name}: 模拟观看av号为{ids[5]["aid"]}的视频投币失败，原因为：{ret["message"]}')
+            logging.warning(f'{biliapi.name}: 模拟观看av号为{ids[nub]["aid"]}的视频投币失败，原因为：{ret["message"]}')
             webhook.addMsg('msg_simple', f'{biliapi.name}:模拟视频观看失败\n')
     except Exception as e: 
         logging.warning(f'{biliapi.name}: 模拟视频观看异常，原因为{str(e)}')
